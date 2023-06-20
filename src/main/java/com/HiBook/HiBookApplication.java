b0f18d8a52bd33eacf6431b4954c106cc6fcf050
eunhye <eunhye>
2023-06-20 17:33:47 +0900

    aws 세팅

--- src/main/java/com/HiBook/HiBookApplication.java
+++ src/main/java/com/HiBook/HiBookApplication.java
@@ -1,13 +1,12 @@
 package com.HiBook;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 //@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 @SpringBootApplication
-public class HiBookApplication {
+public class HiBookApplication extends SpringBootServletInitializer {
 	
 	public static void main(String[] args) {
 		SpringApplication.run(HiBookApplication.class, args);
